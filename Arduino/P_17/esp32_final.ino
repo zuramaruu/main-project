c419ebc1f37f956e1ff2704898f34f7ed392b05e
Iwan Dwi Purwanto <97489791+lilchae@users.noreply.github.com>
2022-06-19 13:20:29 +0700

    data Value added

--- Arduino/P_17/esp32_final.ino
+++ Arduino/P_17/esp32_final.ino
@@ -7,6 +7,11 @@ const char WIFI_PASSWORD[] = "bintangku";
 String HOST_NAME = "http://192.168.1.10/"; // change to your PC's IP address
 String PATH_NAME   = "/post_data.php";
 
+//-- Parse String --//
+String dataIn;
+boolean parsing=false;
+String temperatureSatu, temperatureDua, phValueSatu, phValueDua, fuzzySalinity;
+
 void setup() {
   Serial.begin(9600); 
 
@@ -16,31 +21,58 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
 
+  // -- Data Masuk -- //
+    dataIn="";
 }
 
 void loop() {
-  int randomValue = random(9999);
-  int randomHour = random(0,23);
-  int randomMinute = random(0,59);
-  int randomDay = random(1,31);
-  int randomMonth = random(1,12);
+//  int randomValue = random(9999);
+//  int randomHour = random(0,23);
+//  int randomMinute = random(0,59);
+//  int randomDay = random(1,31);
+//  int randomMonth = random(1,12);
 
+//    String queryString = String("?data=A&date=")+
+//                         String(randomDay)+
+//                         String("%2F")+
+//                         String(randomMonth)+
+//                         String("%2F2022&time=")+
+//                         String(randomHour)+
+//                         String(".")+
+//                         String(randomMinute)+
+//                         String("&value=")+
+//                         String(randomValue);
 
+    if(Serial.available()>0){
+        char inChar = (char)Serial.read();
+        dataIn += inChar;
+        if (inChar == '\n') {parsing = true;}
+    }
+    if(parsing){
+        temperatureSatu = getValue(dataIn, "#", 0);
+        temperatureDua = getValue(dataIn, "#", 1);
+        phValueSatu = getValue(dataIn, "#", 2);
+        phValueDua = getValue(dataIn, "#", 3);
+        fuzzySalinity = getValue(dataIn, "#", 4);
+
+        //-- Empty the Serial --//
+        parsing = false;
+        dataIn = "";
+    }
   String queryString = String("?data=A&date=")+
-          String(randomDay)+
+          String(temperatureSatu)+
           String("%2F")+
-          String(randomMonth)+
+          String(temperatureDua)+
           String("%2F2022&time=")+
-          String(randomHour)+
+          String(phValueSatu)+
           String(".")+
-          String(randomMinute)+
+          String(phValueDua)+
           String("&value=")+
-          String(randomValue);
+          String(fuzzySalinity);
 
   HTTPClient http;
 
@@ -62,5 +94,20 @@ void loop() {
   }
 
   http.end();
-  delay(5000);
+//  delay(5000); //-- Optional
+}
+String getValue(String data, char separator[], int index){
+    int found = 0;
+    int strIndex[] = {0, -1};
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator[0] || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i + 1 : i;
+        }
+    }
+
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
