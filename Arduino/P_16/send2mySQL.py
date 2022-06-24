d4186c28e03222158a9c3a2145ec0909f347b525
Iwan Dwi Purwanto <97489791+iwandwip@users.noreply.github.com>
2022-06-24 16:12:09 +0700

    Serial receive from Arduino

--- Arduino/P_16/send2mySQL.py
+++ Arduino/P_16/send2mySQL.py
@@ -1,8 +1,20 @@
-import mysql.connector
 import time
+import serial
+import mysql.connector
 import datetime
 import random
 
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyACM0',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
+
+ser.isOpen()
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -13,12 +25,31 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-while True:
-  t = datetime.datetime.now()
-  sql = "INSERT INTO data_sensor_a (tanggal , jam, nilai_sensor) VALUES (%s, %s, %s)"
-  val = [("{}/{}/{}".format(t.day, t.month, t.year)),
-                  ("{}:{}".format(t.hour, t.minute)), 
-                  ("{}".format(random.random()))]
-  mycursor.execute(sql, val)
-  mydb.commit()
-  print(mycursor.rowcount, "record inserted.")
+while 1 :
+    tbl_db = ["data_sensor_a", "data_sensor_b", "data_sensor_c", "data_sensor_d",
+                "data_sensor_e", "data_sensor_f", "data_sensor_g", "data_sensor_h",
+                "data_sensor_i"]
+    
+    t = datetime.datetime.now()
+    arduinoData = ser.readline().decode('ascii')
+    #print(arduinoData)
+    
+    x = arduinoData.split(" ")
+    
+    if len(x) == 10 :
+        for i, datas in enumerate(x):
+            if i == 9:
+                break
+            sql = "INSERT INTO "+ str(tbl_db[i])  +" (tanggal , jam, nilai_sensor) VALUES (%s, %s, %s)"
+            val = [("{}/{}/{}".format(t.day, t.month, t.year)),
+                              ("{}:{}".format(t.hour, t.minute)), 
+                              ("{}".format(str(datas)))]
+            
+           #print(val)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            
+            print(mycursor.rowcount, " "+ str(tbl_db[i]) +" record inserted
+            #print(sql, " " + str(tbl_db[i]) +" record inserted.")
+
+            #print("Data ke-{} = {}".format(i+1,datas))
