37c51e98f406ef0a9b83e86fd3be705e6012f3ea
Iwan Dwi Purwanto <97489791+iwandwip@users.noreply.github.com>
2022-07-28 23:25:11 +0700

    coffee

---
+++ Arduino/coffeeRoaster/coffee_roaster/relayHandler.ino
@@ -0,0 +1,31 @@
+int relay[2] = {4, 2};
+
+void relay_Init() {
+  pinMode(relay[0], OUTPUT);
+  pinMode(relay[1], OUTPUT);
+  digitalWrite(relay[0], HIGH);
+  digitalWrite(relay[1], HIGH);
+}
+void LoopTest_Relay() {
+  for (int i = 0; i < 2; i++) {
+    Serial.println("LOOP TEST RELAY UTAMA : " + String(i));
+    digitalWrite(relay[i], LOW);
+    delay(1000);
+    digitalWrite(relay[i], HIGH);
+    delay(1000);
+  }
+}
+
+void relaySatuOn() {
+  digitalWrite(relay[0], LOW);
+}
+void relaySatuOff() {
+  digitalWrite(relay[0], HIGH);
+}
+
+void relayDuaOn() {
+  digitalWrite(relay[1], LOW);
+}
+void relayDuaOff() {
+  digitalWrite(relay[1], HIGH);
+}
