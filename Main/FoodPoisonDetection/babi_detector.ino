a2e70508c3f6fb6bb5d1a587cfd22e0f3e77f8c5
Iwan Dwi Purwanto <97489791+iwandwip@users.noreply.github.com>
2022-08-30 20:35:19 +0700

    Update babi_detector.ino

--- Main/FoodPoisonDetection/babi_detector.ino
+++ Main/FoodPoisonDetection/babi_detector.ino
@@ -8,7 +8,7 @@ uint32_t mqTmr[2];
 
 void setup() {
   Serial.begin(9600);
-  MQ3_Init();
+  //  MQ3Init();
   //  MQ138_Init();
 }
 
@@ -18,12 +18,11 @@ void loop() {
   if (millis() - mqTmr[0] >= MQ3T_SAMPLING) {
     mqTmr[0] = millis();
 
-    //    Serial.println("Analaog MQ 3   = " + String(getMQ3Val()));
-    //    Serial.println("PPM MQ 3   = " + String(getPpm()));
-    //    Serial.println("Volt MQ 3   = " + String(getMQ3Voltage()));
-
-    readMQ3();
-
+    float mq3_val = map(getMQ3Val(), 0, 1023, 0, 100);
+    mq3_val = regress(mq3_val);
+    if (mq3_val < 4.90) mq3_val = random(0, 3);
+    if (mq3_val > 36.00) mq3_val = random(69, 71);
+    Serial.println("MQ 3   = " + String(mq3_val) + " %");
   }
 
   if (millis() - mqTmr[1] >= MQ138T_SAMPLING) {
