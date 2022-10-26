c512f850d6bd07bb60ebe9101b45be57b7b5ed8f
Iwan Dwi Purwanto <97489791+iwandwip@users.noreply.github.com>
2022-10-27 00:57:14 +0700

    Update

--- Main/Flask-py/relay-btn/Board/flask-try.ino
+++ Main/Flask-py/relay-btn/Board/flask-try.ino
@@ -1,5 +1,11 @@
 #include "WiFi.h"
 #include "HTTPClient.h"
+#include "Wire.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
 #define   SSID_WIFI     "Polinema Hotspot LT8 Barat"
 #define   PASS_WIFI     ""
@@ -19,6 +25,8 @@ uint8_t wifiStatus = 0;
 uint32_t sysTmr[3];
 uint8_t state[4] = {0, 0, 0, 0};
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup() {
   Serial.begin(115200);
   for (uint8_t i = 0; i < 4; i++) {
@@ -26,6 +34,9 @@ void setup() {
   }
   pinMode(LED_BUILTIN, OUTPUT);
   InitWifi();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) return; // Address 0x3D for 128x64
+  
 }
 
 void loop() {
@@ -35,6 +46,26 @@ void loop() {
     for (uint8_t i = 0; i < 4; i++) {
       state[i] = digitalRead(btnPin[i]);
     }
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+
+    display.setCursor(0, 10);
+    display.println("State 1 = ");
+
+    display.setCursor(11, 10);
+    if (getState(0)) display.println("ON");
+    else display.println("OFF");
+
+    display.setCursor(0, 20);
+    display.println("State 2 = ");
+
+    display.setCursor(11, 20);
+    if (getState(1)) display.println("ON");
+    else display.println("OFF");
+
+    display.display();
 
     sysTmr[2] = millis();
   }
